462fffb94073c67760b61e7724d6f2ed906bfa39
vantam8300 <vantam8300@gmail.com>
2022-12-04 21:22:59 -0800

    update seeds.sql file

--- db/seeds.sql
+++ db/seeds.sql
@@ -2,12 +2,12 @@ INSERT INTO department (name)
 VALUES ("IT"), ("HR");
 
 INSERT INTO role (title, salary, department_id) 
-VALUES ("engineer", 1000, 1),
-("intern", 100, 1),
+VALUES ("Engineer", 1000, 1),
+("Intern", 100, 1),
 ("Recruiter", 300, 2);
 
 
 INSERT INTO employee (first_name, last_name, role_id) 
-VALUES ("Van Tam", "Chau1", 2),
-("Van Tam", "Chau2", 1),
-("Van Tam", "Chau3", 3);
+VALUES ("John", "Doe", 2),
+("Mike", "Chan", 1),
+("Ashley", "Rodriguez", 3);
