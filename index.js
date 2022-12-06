70fa5b96ec74d549f6aebcc909c655d99a53e435
vantam8300 <vantam8300@gmail.com>
2022-12-05 18:16:58 -0800

    Adding additional functionality

--- index.js
+++ index.js
@@ -15,6 +15,13 @@ const initalquestions = [
             "Add A Role",
             "Add An Employee",
             "Update An Employee Role",
+            "Update Employee Managers",
+            "View Employees By Manager",
+            "View Employees By Department",
+            "Delete Department",
+            "Delete Role",
+            "Delete Employee",
+            "View Budget By Department",
             "Quit",
         ]
     }
@@ -69,6 +76,34 @@ function startApp() {
 
                 case "Update An Employee Role":
                     updateRole();
+                    break;
+
+                case "Update Employee Managers":
+                    updateEmployeeManagers();
+                    break;
+
+                case "View Employees By Manager":
+                    viewEmployeeByManager();
+                    break;
+
+                case "View Employees By Department":
+                    viewEmployeesByDepartment();
+                    break;
+
+                case "Delete Department":
+                    deleteDepartment();
+                    break;
+
+                case "Delete Role":
+                    deleteRole();
+                    break;
+
+                case "Delete Employee":
+                    deleteEmployee();
+                    break;
+
+                case "View Budget By Department":
+                    viewBudgetByDeparment();
                     break;
 
                 case "Quit":
@@ -163,7 +198,6 @@ function addRole() {
     });
 
 }
-
 
 function addEmployee() {
     db.execute('SELECT * FROM role', (err, roleResults) => {
@@ -298,4 +332,256 @@ function updateRole() {
         })
     })
 
+}
+
+function updateEmployeeManagers() {
+
+
+    db.execute('SELECT * FROM employee', (err, employeeResults) => {
+        // list employee name
+        let employees = [];
+        for (employee of employeeResults) {
+            employees.push(employee.first_name + " " + employee.last_name);
+        }
+        let managers = ["None", ...employees];
+        inquirer.prompt([
+            {
+                type: "list",
+                name: "employee",
+                message: "Which employee do you want to update?",
+                choices: employees,
+            },
+            {
+                type: "list",
+                name: "manager",
+                message: "Which manager do you want to assign the selected employee?",
+                choices: managers,
+            }
+        ]).then(answer => {
+
+            // find manager id match with chosen manager name
+            let managerId;
+            let fullName;
+            for (let i = 0; i < employeeResults.length; i++) {
+                fullName = employeeResults[i].first_name + " " + employeeResults[i].last_name;
+                if (fullName == answer.manager) {
+                    managerId = employeeResults[i].id;
+                    break;
+                }
+            }
+
+            // if user chose "none" for manager' name set manager id to null
+            if (!managerId) {
+                managerId = null;
+            }
+
+            // find employee id match with chosen employee name 
+            let employeeId;
+            for (let i = 0; i < employeeResults.length; i++) {
+                fullName = employeeResults[i].first_name + " " + employeeResults[i].last_name;
+                if (fullName == answer.employee) {
+                    employeeId = employeeResults[i].id;
+                    break;
+                }
+            }
+            // update employee's manager
+            db.execute("UPDATE employee SET manager_id = ? where id = ?", [managerId, employeeId]);
+            console.log("Updated employee's manager");
+            // Start intial Questions Again
+            startApp();
+        });
+    })
+}
+
+function viewEmployeeByManager() {
+
+    db.execute('SELECT * FROM employee', (err, employeeResults) => {
+        // list employee name
+        let managers = [];
+        for (employee of employeeResults) {
+            managers.push(employee.first_name + " " + employee.last_name);
+        }
+
+        inquirer.prompt([
+            {
+                type: "list",
+                name: "manager",
+                message: "Which manager do you want to select?",
+                choices: managers,
+            },
+        ]).then(answer => {
+
+            // find manager id match with chosen manager name
+            let managerId;
+            let fullName;
+            for (let i = 0; i < employeeResults.length; i++) {
+                fullName = employeeResults[i].first_name + " " + employeeResults[i].last_name;
+                if (fullName == answer.manager) {
+                    managerId = employeeResults[i].id;
+                    break;
+                }
+            }
+
+            db.execute('SELECT employee.id, employee.first_name, employee.last_name, role.title FROM employee LEFT JOIN role ON employee.role_id = role.id where employee.manager_id = ? ', [managerId], (err, results) => {
+                console.table(results)
+                // Start intial Questions Again
+                startApp();
+            })
+
+        });
+    })
+}
+
+function viewEmployeesByDepartment() {
+    db.execute('SELECT * FROM department', (err, results) => {
+        inquirer.prompt([
+            {
+                type: "list",
+                name: "department",
+                message: "Which department do you want to select?",
+                choices: results,
+            },
+        ]).then(answer => {
+
+            // find department id match with chosen department name 
+            let departmentId;
+            for (let i = 0; i < results.length; i++) {
+                if (results[i].name == answer.department) {
+                    departmentId = results[i].id;
+                }
+            }
+
+            db.execute("SELECT employee.id, employee.first_name, employee.last_name, role.title, role.salary FROM employee INNER JOIN role ON employee.role_id = role.id INNER JOIN department ON department.id = role.department_id  where department.id = ?", [departmentId], (err, result) => {
+                console.table(result);
+                startApp();
+            })
+
+        })
+    })
+}
+
+function deleteDepartment() {
+    db.execute('SELECT * FROM department', (err, results) => {
+        inquirer.prompt([
+            {
+                type: "list",
+                name: "department",
+                message: "Which department do you want to delete?",
+                choices: results,
+            },
+        ]).then(answer => {
+
+            // find department id match with chosen department name 
+            let departmentId;
+            for (let i = 0; i < results.length; i++) {
+                if (results[i].name == answer.department) {
+                    departmentId = results[i].id;
+                }
+            }
+
+            db.execute("DELETE FROM department where id = ?", [departmentId])
+            console.log(`${answer.department} department is deleted`)
+            startApp();
+        })
+
+    })
+}
+
+function deleteRole() {
+    db.execute('SELECT * FROM role', (err, roleResults) => {
+        // list role title
+        let roles = [];
+        for (role of roleResults) {
+            roles.push(role.title);
+        }
+
+        inquirer.prompt([
+            {
+                type: "list",
+                name: "role",
+                message: "Which role do you want to delete?",
+                choices: roles,
+            }
+        ]).then(answer => {
+
+            // find role id match with chosen role title 
+            let roleId;
+            for (let i = 0; i < roleResults.length; i++) {
+                if (roleResults[i].title == answer.role) {
+                    roleId = roleResults[i].id;
+                }
+            }
+
+            // delete  role
+            db.execute("DELETE FROM role WHERE id = ?", [roleId]);
+            console.log(`${answer.role} role is deleted`);
+            // Start intial Questions Again
+            startApp();
+        });
+    })
+}
+
+function deleteEmployee() {
+    db.execute('SELECT * FROM employee', (err, employeeResults) => {
+        // list employee name
+        let employees = [];
+        for (employee of employeeResults) {
+            employees.push(employee.first_name + " " + employee.last_name);
+        }
+
+        inquirer.prompt([
+            {
+                type: "list",
+                name: "employee",
+                message: "Which employee do you want to delete?",
+                choices: employees,
+            },
+        ]).then(answer => {
+
+            let fullName;
+            let employeeId;
+            for (let i = 0; i < employeeResults.length; i++) {
+                fullName = employeeResults[i].first_name + " " + employeeResults[i].last_name;
+                if (fullName == answer.employee) {
+                    employeeId = employeeResults[i].id;
+                    break;
+                }
+            }
+
+            db.execute('DELETE FROM employee WHERE id = ?', [employeeId])
+            console.log(`${answer.employee} is deleted`)
+            // Start intial Questions Again
+            startApp();
+        })
+
+    });
+}
+
+function viewBudgetByDeparment() {
+    db.execute('SELECT * FROM department', (err, departmentResults) => {
+        inquirer.prompt([
+            {
+                type: "list",
+                name: "department",
+                message: "Which department do you want to select?",
+                choices: departmentResults,
+            },
+        ]).then(answer => {
+
+            // find department id match with chosen department name 
+            let departmentId;
+            for (let i = 0; i < departmentResults.length; i++) {
+                if (departmentResults[i].name == answer.department) {
+                    departmentId = departmentResults[i].id;
+                }
+            }
+
+            db.execute("SELECT department.id, department.name, sum(role.salary) as budget FROM department JOIN role ON department.id = role.department_id JOIN employee ON employee.role_id = role.id WHERE department.id = ?", [departmentId], (err, results) => {
+                console.table(results)
+                startApp();
+            })
+            
+        })
+
+    })
 }
